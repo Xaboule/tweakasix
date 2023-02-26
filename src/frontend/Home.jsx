ea742e9e7c1d02aac4ab6833d053bfe80986e4f8
Xaboule <xaboule@gmail.com>
2023-02-26 19:36:56 +0100

    logo import

--- src/frontend/Home.jsx
+++ src/frontend/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import logo from '../assets/img/tweakasix.png'
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-        <h1>Welcome to</h1>
-        
+    <div className='home'>
+        <h1>Welcome <br/>to</h1>
+    <img src={logo} alt="logo" className='logo'/>
+
+
+    
         </div>
   )
 }
