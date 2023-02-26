eb413e867d864821e9e5979947ab9a0b89ea7ee8
Barren <xaboule@gmail.com>
2023-02-26 18:18:08 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from './frontend/Home';
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+<Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home/>}
+          />
+          {/* <Route
+            path="/favoris"
+            element={<Favoris eventArrayFromAPI={eventArrayFromAPI} />}
+          /> */}
+          {/* <Route path="/aboutus" element={<Aboutus/>} /> */}
+          {/* <Route path="/event/:id" element={<Detailspretext />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
