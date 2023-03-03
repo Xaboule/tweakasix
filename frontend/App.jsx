13d11c7ddb3aaf3df371677389b062b459a26e1f
Xaboule <xaboule@gmail.com>
2023-03-03 15:43:37 +0100

    split back and move packages

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Home from './frontend/Home';
+import Home from './src/components/Home';
 
 function App() {
 
